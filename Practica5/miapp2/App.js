759aa3ca18b37cdde6113a5613074247870ce576
Maria-Palma0731 <123046449@upq.edu.mx>
2026-06-10 20:37:00 -0600

    Practica7

--- Practica5/miapp2/App.js
+++ Practica5/miapp2/App.js
@@ -4,19 +4,37 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import {Saludo} from './components/Saludo';
 import {Saludo2} from './components/Saludo2';
 import {Perfil} from './components/Perfil';
+
+
+
 /* Zona2: Main - componentes */
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.container} >
       <Image source={require('./assets/wave.png')}/>
       <Text>Hola Mundo React Native</Text>
-      <Text> ----------------------------</Text>
+    {/*   <Text> ----------------------------</Text>
       <Saludo></Saludo>
       <Saludo/>
       <Text> ----------------------------</Text>
       <Saludo2/>
-      <Text> ----------------------------</Text>
-      <Perfil/>
+      <Text> ----------------------------</Text> */}
+      <Perfil style={styles.tarjetaVerde} 
+      nombre="Lupita" 
+      carrera="ComerScio" 
+      materia="algo" 
+      cuatri="6"/>
+      <Perfil style={styles.tarjetaRoja} 
+      nombre="Maria" 
+      carrera="Sistemas computacionales" 
+      materia="Programacion Móvil" 
+      cuatri="9"/>
+      <Perfil style={styles.tarjetaVerde} 
+      nombre="Angeles" 
+      carrera="ComerScio" 
+      materia="algo" 
+      cuatri="6"/>
+      
       <StatusBar style="auto" />
     
     </View>
@@ -29,6 +47,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row', /*Posicionamiento horizontal */ /*column-reverse invierte el orden de los componentes */
   },
+    tarjetaVerde: {
+      backgroundColor: 'green',
+    },
+    tarjetaRoja: {
+      backgroundColor: 'red',
+    },
+  
 });
--- Practica5/miapp2/components/Perfil.js
+++ Practica5/miapp2/components/Perfil.js
@@ -1,12 +1,65 @@
-import { Text, View } from "react-native";
+/*Perfil usando desestructurado */
+/*Renderizado condicional */
+ /* Etiquetas de fragmento con etiquetas vacias */
+import { Text, View, Button, StyleSheet} from "react-native";
+import React,{useState} from "react";
 
-export const Perfil = () => {
+export const Perfil = ({nombre,carrera, materia,cuatri,style}) => {
+    const [mostrar, setMostrar] = useState(false)
+
     return (
-        <View>
-            <Text>Nombre: Maria Palma</Text>
-            <Text>Carrera: Sistemas computacionales</Text>
-            <Text>Materia: Programacion Móvil</Text>
-            <Text>Cuatrimestre: 9 Cuatrimestre</Text>
+        <View style={[styles.tarjeta, style]}>
+            <Text style={styles.nombre}>{nombre}</Text>
+            {mostrar &&
+            <>      
+            <Text style={styles.carrera}>{carrera}</Text>
+            <Text style={styles.otroTexto}>{materia}</Text>
+            <Text style={styles.otroTexto}>{cuatri}</Text>
+            </>
+            }
+            <Button title= "Ver perfil" onPress={() => setMostrar(!mostrar)}/>
+
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    nombre: {
+        fontSize: 24,
+        fontWeight: 600,
+        textTransform: 'uppercase',
+    },
+    tarjeta: {
+        borderWidth: 2,
+        padding: 15,
+        margin: 10,
+    },
+    carrera: {
+        fontSize: 18,
+        color: 'gray',
+        frontfamily: 'Roboto',
+    },
+    otroTexto: {
+        fontSize: 12,
+        frontfamily: 'Roboto',
+        frontStyle: 'italic',
+    },
+
+});
+
+
+
+/* Perfil usando props.
+import { Text, View } from "react-native";
+
+export const Perfil = (props) => {
+    return (
+        <View>
+            <Text>{props.nombre}</Text>
+            <Text>{props.carrera}</Text>
+            <Text>{props.materia}</Text>
+            <Text>{props.cuatri}</Text>
+        </View>
+    );
+}; */
+
